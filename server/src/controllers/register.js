1c8c125d170ca4e64ee07055e270f4502027c4ec
riaanggraini99 <ria.anggraini@students.amikom.ac.id>
2018-07-08 00:38:48 +0700

    fixing register route bugs

--- server/src/controllers/register.js
+++ server/src/controllers/register.js
@@ -1,10 +1,11 @@
 const {User}  = require('../../models/User')
-const register = require('../routes/register');
+//const register = require('../routes/register');
 
 module.exports = {
     async register(req, res){
         try {
         const user = await User.create(req.body)
+        res.send(user.toJSON())
     }catch(err){
         //if email already exist
         res.status(400).send({
--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -1,18 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const { userController } = require('../controllers/register.js');
+const userController  = require('../controllers/register.js');
 //const register = require('../controllers/register');
-const { body, validationResult } = require('express-validator/check');
+//const { body, validationResult } = require('express-validator/check');
 
 
-router.post('/',(req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(422).json(response(false, errors.array()));
-      }
-      userController.create(req, res);
-    }
-  );
+router.post('/', userController.register)
 
-module.exports = router
+
+
+module.exports = router;
 
